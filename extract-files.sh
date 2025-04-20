7b92d34bdc3b72d14df53e66863c44ae4f261015
opensourcefreak <john@snurr.nu>
2025-04-20 16:07:02 +0200

    Add missing ril blob We can now send sms again

--- extract-files.sh
+++ extract-files.sh
@@ -7,14 +7,6 @@
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/hw/com.qti.chi.override.so)
-            xxd -p "${2}" | tr -d \\n > "${2}".hex
-            # NOP CONNECT_RILD
-            sed -i "s/800640f9e17800948001003482dbfff0c3ddff90a5dcffb0e603002a/1f2003d51f2003d51f2003d51f2003d51f2003d51f2003d51f2003d5/g" "${2}".hex
-            sed -i "s/4238089163f00891a5241391a4118052e0031f2a210080528dfeff97800640f9/1f2003d51f2003d51f2003d51f2003d51f2003d51f2003d51f2003d5800640f9/g" "${2}".hex
-            xxd -r -p "${2}".hex > "${2}"
-            rm "${2}".hex
-            ;;
         vendor/lib64/unihal_android.so)
             grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
             ;;
